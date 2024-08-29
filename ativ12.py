974fb94140516d8b9b53fb2b4318de440c5191fa
jeanpietro <jeanpietrokeylle13@gmail.com>
2024-08-29 09:19:11 -0300

    Desafios 12,20,21,22,23,24,26

---
+++ ativ12.py
@@ -0,0 +1,9 @@
+# Solicitando e recebendo os numeros para verificar se o segundo numero e menor que o primeiro
+num1 = input('Digite um número: ')
+num1 = float(num1)
+num2= input('Digite um número: ')
+num2 = float(num2)
+
+# Exibindo e fazendo a verificação dos números recebidos
+if num2<num1:
+    print(f'O segundo número e menor que o primeiro')
